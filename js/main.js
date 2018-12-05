88a8bcbd724ad01942b787307eda3dcfc102c2f5
William Metcalfe <willymetcalfe98@gmail.com>
2018-12-04 23:17:27 -0500

    Create landing screen, main content and footer for Desktop @ 1920x1080, add some JQuery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 /* Brantford Living JS */
+
+// Show/Hide Main Nav
+$(document).ready(function() {
+    $(".menu").click(function(){
+        $(".mainNav").toggle();
+    });
+});
+
+// Auto Scroll Function
+$(".findOut").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#brantfordContainer").offset().top
+    }, 1000);
+});
