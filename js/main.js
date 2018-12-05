5e8d2fbfeefb4eb677b3f37288e8374b7ed37c0a
William Metcalfe <willymetcalfe98@gmail.com>
2018-12-05 16:21:48 -0500

    Add scrollTo functions for main nav and footer nav

--- js/main.js
+++ js/main.js
@@ -13,3 +13,27 @@ $(".findOut").click(function() {
         scrollTop: $("#brantfordContainer").offset().top
     }, 1000);
 });
+
+$(".home").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".logo").offset().top
+    }, 1000);
+});
+
+$(".about").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#brantfordContainer").offset().top
+    }, 1000);
+});
+
+$(".choose").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#chooseBrantford").offset().top
+    }, 1000);
+});
+
+$(".commute").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#commuteTitle").offset().top
+    }, 1000);
+});
